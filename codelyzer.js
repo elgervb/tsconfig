a612906829712364abc65c6e805ac2fcee6d1c2e
Elger van Boxtel <elger.van.boxtel@isatis-health.nl>
2017-04-21 13:15:57 +0200

    extract codelyzer from main config

---
+++ codelyzer.js
@@ -0,0 +1,29 @@
+"use strict"
+
+module.exports = {
+  "rulesDirectory": [
+    "node_modules/codelyzer",
+  ],
+  "rules": {
+    /**
+     * CodeLyzer ruling
+     */
+    "directive-selector": [ true, "attribute", "smt", "camelCase" ],
+    "component-selector": [ true, "element", "smt", "kebab-case" ],
+    "use-input-property-decorator": true,
+    "use-output-property-decorator": true,
+    "use-host-property-decorator": true,
+    "no-attribute-parameter-decorator": true,
+    "no-input-rename": true,
+    "no-output-rename": true,
+    "no-forward-ref": true,
+    "use-life-cycle-interface": true,
+    "use-pipe-transform-interface": true,
+    "pipe-naming": [ true, "camelCase", "smt" ],
+    "component-class-suffix": true,
+    "directive-class-suffix": true,
+    "templates-use-public": true,
+    "no-access-missing-member": true,
+    "invoke-injectable": true,
+  },
+}
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict"
 
 module.exports = {
-  "rulesDirectory": [
-    "node_modules/codelyzer",
-  ],
   "rules": {
     /**
      * TypeScript Specific
@@ -207,26 +204,5 @@ module.exports = {
       "check-type",
       "check-typecast",
     ],
-
-    /**
-     * CodeLyzer
-     */
-    "directive-selector": [ true, "attribute", "smt", "camelCase" ],
-    "component-selector": [ true, "element", "smt", "kebab-case" ],
-    "use-input-property-decorator": true,
-    "use-output-property-decorator": true,
-    "use-host-property-decorator": true,
-    "no-attribute-parameter-decorator": true,
-    "no-input-rename": true,
-    "no-output-rename": true,
-    "no-forward-ref": true,
-    "use-life-cycle-interface": true,
-    "use-pipe-transform-interface": true,
-    "pipe-naming": [ true, "camelCase", "smt" ],
-    "component-class-suffix": true,
-    "directive-class-suffix": true,
-    "templates-use-public": true,
-    "no-access-missing-member": true,
-    "invoke-injectable": true,
   },
 }
